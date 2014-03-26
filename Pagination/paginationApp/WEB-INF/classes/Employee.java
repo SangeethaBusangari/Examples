0172c003ab60ffb51d262cc4f3c52245a8c63253
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-03-26 12:15:41 +0530

    Create Employee.java

---
+++ Pagination/paginationApp/WEB-INF/classes/Employee.java
@@ -0,0 +1,25 @@
+class Employee  
+{
+	int no;
+	String name,email;
+	public void setNo(int no)
+	{this.no=no;}
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public void setEmail(String  mail)
+	{
+		email=mail;
+	}
+	public int getNo()
+	{return no;}
+	public String getName()
+	{
+		return name;
+	}
+	public String getEmail()
+	{
+		return email;
+	}
+}
